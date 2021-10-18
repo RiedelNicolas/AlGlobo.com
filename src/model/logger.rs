0222a4786740cd971419853238a89e6e57d6c29c
RiedelNicolas <nariedel99@gmail.com>
2021-10-18 18:05:15 -0300

    implementation of logger

--- src/model/logger.rs
+++ src/model/logger.rs
@@ -1,13 +1,40 @@
-enum MessageType {
-    Error,
-    Warning,
-    Info
-}
+use super::message;
+use std::io::Write;
+use std::fs::File;
+use std::fs;
 
-pub struct Message {
-    type:MessageType,
-    text:String
-}
 
 pub struct Logger {
+    rx: std::sync::mpsc::Receiver<message::Message>,
+    tx: std::sync::mpsc::Sender<message::Message>,
+    log_file:File
+}
+
+impl Logger {
+    pub fn new() -> Logger {
+        let (sender, receiver) = std::sync::mpsc::channel::<message::Message>();
+        Logger {
+            rx: receiver,
+            tx: sender,
+            log_file : fs::OpenOptions::new()
+                .write(true)
+                .append(true)
+                .create(true)
+                .open("files/log_file.txt")
+                .unwrap()
+        }
+    }
+    pub fn print_received(&mut self) {
+        let received = self.rx.recv().unwrap();
+        println!("{}", received.generate_string() );
+        let r = write!(self.log_file, "{}\n", &received.generate_string() );
+        match r {
+            Ok(v) => v,
+            Err(e) => println!("Error found trying to write the logfile : {}",e)
+        }
+    }
+    // returns an instances of a channel sender.
+    pub fn get_transmitter(&self) -> std::sync::mpsc::Sender<message::Message> {
+        self.tx.clone()
+    }
 }
--- src/model/message.rs
+++ src/model/message.rs
@@ -1,10 +1,46 @@
+use super::message_type::MessageType;
+extern crate chrono;
+use chrono::{DateTime, Utc};
+
 pub struct Message {
-    type:MessageType,
+    message_type : MessageType,
     text:String
 }
 
-enum MessageType {
-    Error,
-    Warning,
-    Info
+impl Message {
+
+    //creo los 3 constructores 
+    pub fn new_error(in_text:String) -> Message {
+        Message {
+            text: in_text,
+            message_type: MessageType::Error
+        }
+    }
+
+    pub fn new_info(in_text:String) -> Message {
+        Message {
+            text: in_text,
+            message_type: MessageType::Info
+        }
+    }
+
+    pub fn new_warning(in_text:String) -> Message {
+        Message {
+            text: in_text,
+            message_type: MessageType::Warning
+        }
+    }
+
+    pub fn generate_string (&self)->String {
+    
+        let aux : String;
+        let now: DateTime<Utc> = Utc::now();
+
+        match self.message_type {
+            MessageType::Info => aux = "INF".to_string(),
+            MessageType::Error => aux = "ERR".to_string(),
+            MessageType::Warning => aux = "WAR".to_string(),
+        }
+        format!("{}-{}-{}", aux, now.format("%e %B %Y %T") ,self.text)
+    }
 }
