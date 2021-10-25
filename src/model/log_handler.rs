fcc4f07c9dcbac1f9c13bd4ba62746fa0153236c
RiedelNicolas <nariedel99@gmail.com>
2021-10-24 22:50:57 -0300

    documento log_handler

--- src/model/log_handler.rs
+++ src/model/log_handler.rs
@@ -6,12 +6,15 @@ use std::fs::File;
 use std::fs;
 use std::thread::{self, JoinHandle};
 
+/// Represents a log system.
 pub struct LogHandler {
     tx: std::sync::mpsc::Sender<message::Message>,
     logger: Option<JoinHandle<()>>,
 }
 
 impl LogHandler {
+    /// Generates an instances of logHandler
+    /// Receives a path where the log file is going to be constructed.
     pub fn new( path : &str ) -> LogHandler {
         let (sender, receiver) = std::sync::mpsc::channel::<message::Message>();
         let log_file = fs::OpenOptions::new()
@@ -26,7 +29,8 @@ impl LogHandler {
             logger: Some(thread::spawn( move || LogHandler::print_received(receiver, log_file)))
         }
     }
-    
+    /// Prints all the messages that are waiting in the queue
+    /// Warning : Blocking method
     fn print_received(rx: std::sync::mpsc::Receiver<message::Message>, mut log_file: File) {
         loop {
             match rx.recv() {
@@ -49,12 +53,14 @@ impl LogHandler {
         }
     }
 
-    // returns an instances of a sender.
+    /// returns an instance of a trasmitter, used to send messages to the logger.
     pub fn get_transmitter(&self) -> Logger {
         Logger::new( &self.tx.clone() )
     }
 
+    /// Blocking, waits until all the messages in the queue are printed. 
     pub fn join(self) {
         if let Some(logger) = self.logger { let _ = logger.join(); }
     }
+    
 }
